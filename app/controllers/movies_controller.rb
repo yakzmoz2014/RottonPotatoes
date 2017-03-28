73a66f3b9c8953306f4f89e62ded424d5dce42ed
wangdonghui <wangdonghuiibt@gmail.com>
2017-03-28 15:26:21 +0800

    when created a movie, favorite it automatically

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,6 +15,7 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
     @movie.user = current_user
     if @movie.save
+      current_user.favorite!(@movie)
       redirect_to movies_path
     else
       render :new
