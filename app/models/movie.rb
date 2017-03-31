01ff1592d36edfa49961853bf4011a0eab4e1871
wangdonghui <wangdonghuiibt@gmail.com>
2017-04-01 00:57:17 +0800

    cancel image validates

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,11 @@ class Movie < ApplicationRecord
   has_many :members, through: :movie_relationships, source: :user
   validates :title, presence: true
   validates :description, presence: true
-  validates :image, presence: true
+  
 
   has_attached_file :image, styles: {
     medium: "300x300>", thumb: "100x100>"
     }, default_url: "/images/:style/missing.png"
-    
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
